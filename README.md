87e092af292adaaeb918dc080d2af9584c65357b
Allan Barcelos <allan@barcelos.dev>
2025-02-11 09:23:56 -0500

    api finished

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ docker run --name my-database -e MYSQL_ROOT_PASSWORD=123456 \
 ```
 
 - You can use the defaulkt port in your host "3306" but if you have problem change to other
+
+
+
+
+# About Midterm
+1 + 3 partes
+
+1) read the requirements (30 min)
+2) questions (15 or 20) (30 min)
+3) project (2h) + github 
+    3.1) create a feature to read a single user (1 commit), the message need be the description "create a feature to read a single user"
+    3.2) create the list page
+    3.3) fix the API communication issue
+4) collaboration (1h)
--- api/index.js
+++ api/index.js
@@ -1,21 +1,19 @@
 // NODEJS
 
 // Declare the libraries
-
-const express = require("express");
-const mysql = require("mysql2/promise");
+const express = require("express"); // minimalist framework
+const mysql = require("mysql2/promise"); // mysql database library
 
 const app = express();
 const port = 3000;
 
-
 const dbConfig = {
-    host: "localhost",
-    user: "in-class-user",
-    password: "123456",
-    database: "in-class-db",
-    port: 3310 // IN MY CASE YOU CAN USE 3306
-}
+  host: "localhost",
+  user: "in-class-user",
+  password: "123456",
+  database: "in-class-db",
+  port: 3310, // IN MY CASE YOU CAN USE 3306
+};
 app.use(express.json());
 
 // HTTP VERBS: POST, GET, PUT, PATCH, OPTIONS
@@ -26,7 +24,72 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "API is runing" });
 });
 
+// POST create / save in database
+app.post("/message", async (req, res) => {
+  const { name, email, message } = req.body;
 
-app.listen(port, () => {
+  if (!name || !email || !message) {
+    return res.status(400).json({ error: "All field are required" });
+  }
+
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    const query = "INSERT INTO users (name, email, message) VALUES (?, ?, ?)";
+    await conn.execute(query, [name, email, message]);
+    await conn.end();
+
+    res.status(201).json({ message: "Created with success" });
+  } catch (e) {
+    res.status(500), json({ error: `Something happens in the server: ${e}` });
+  }
+});
+
+// GET, list the messages; we can use the same route becasue they are differents protocols
+app.get("/message", async () => {
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    const [rows] = await conn.execute("SELECT * FROM messages"); // an object { ..., rows: [{..}]}
+    await conn.end();
+    res.status(200).json(rows); // [ {name, email, message} ]
+  } catch (e) {
+    res.status(500).json({ error: `Fail: ${e}` });
+  }
+});
+
+async function initDatabase() {
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    const [tables] = await conn.query("SHOW TABLES like 'messages'"); // an object {..., tables: [ ... ]}
+
+    if (tables.length === 0) {
+      const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS messages (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(200) NOT NULL,
+          email VARCHAR(200) NOT NULL,
+          message TEXT NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+      `;
+
+      await conn.query(createTableQuery);
+      console.log("Table created");
+    } else {
+      console.log("Table already created");
+    }
+
+    await conn.end();
+  } catch (e) {
+    console.error(`Database error: ${e}`);
+    process.exit(1);
+  }
+}
+
+
+
+initDatabase().then(() => {
+  app.listen(port, () => {
     console.log(`The server is runing, PORT: ${port}`);
+  });
 })
+
